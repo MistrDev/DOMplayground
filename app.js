0ddeb6115011b93b4a0ebfd4693026789c6319cc
MistrDev <cwcorster123@gmail.com>
2021-01-18 08:36:48 -0600

    made the raandom color block a global var

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
 button.addEventListener('click', function () {
+  const newColor = randomColor();
+  document.body.style.backgroundColor = newColor;
+  h1.innerText = newColor;
+})
+
+
+const randomColor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  const newColor = `rgb(${r}, ${g}, ${b})`;
-  document.body.style.backgroundColor = newColor;
-
-})
+    return `rgb(${r}, ${g}, ${b})`;  
+}
