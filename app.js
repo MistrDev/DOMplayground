7020a0c8eb1e9b59509aa005fcc2616e55a3c6ba
MistrDev <cwcorster123@gmail.com>
2021-01-27 09:32:50 -0600

    got it all done you can now post a comment displaying your username!

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ button.addEventListener('click', function () {
   h3.innerText = newColor;
 })
 
-// This function gives us a random color and returns it in rgb 
+// This function gives us a random color and returns it in rgb also a global function for a random color
 const randomColor = () => {
   const R = Math.floor(Math.random() * 256);
   const G = Math.floor(Math.random() * 256);
@@ -16,7 +16,7 @@ const randomColor = () => {
 }
 
 
-// ALL buttons on page change colors when clicked
+// ALL buttons on page change colors when clicked used the randomColor function
 const buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
@@ -28,15 +28,23 @@ for (let button of buttons) {
 
 
 // this will take the users input and make a <ul> while preventing the default action
-const comments = document.querySelector('#comments');
-comments.addEventListener('submit' , function(e) {
+const commentForm = document.querySelector('#commentForm');
+commentForm.addEventListener('submit' , function(e) {
 e.preventDefault();
-const username = comments.elements.username.value;
-const comments = comments.elements.comment.value;
-
-const newComment = document.createElement('li');
-const bTag = document.createElement('b');
-bTag.append(username)
-newComment.append(bTag);
-newComment.append(`- ${comments}`)
+const usernameInput = commentForm.elements.username;
+const commentsInput = commentForm.elements.comment;
+addComment(usernameInput.value ,commentsInput.value)
+// empty strings to reset the inputs after you post a comment
+usernameInput.value = '';
+commentsInput.value = '';
 });
+
+// this makes a new <b> tag and <li> and posts the users comment below
+const addComment = (username,comments) => {
+ const newComment = document.createElement('li');
+ const bTag = document.createElement('b');
+ bTag.append(username)
+ newComment.append(bTag);
+ newComment.append(`- ${comments}`)
+ commentsContainer.append(newComment);
+}
